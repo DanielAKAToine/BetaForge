97eedc26e6e770fb98c2901ba90817cfde435293
DanielAKAToine <daniel.chaveiro1997@gmail.com>
2026-06-19 19:52:58 +0100

    atualização das condições do login para só deixar fazer login depois de ativar o email

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,61 @@
 import { useState } from "react";
 import styles from "./Login.module.css";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/config";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 
 export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
 
-    const handleSubmit = (e) => {
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
         setError("");
+        setLoading(true);
 
         if (!email || !password) {
             setError("Please fill in all fields.");
+            setLoading(false);
             return;
         }
+        setLoading(true);
 
-        alert(`Trying to log in with ${email}`);
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+
+            if (!user.emailVerified) {
+                setError("Please verify your email before logging in.");
+                toast.warn("Please verify your email before logging in.");
+                await signOut(auth);
+                setLoading(false);
+                return;
+            }
+            toast.success("Login successful! Welcome to BetaForge!");
+            navigate("/");
+        }
+        catch (err) {
+            console.error(err);
+
+            if (err.code === "auth/invalid-credential" || err.code === "auth/user-not-found") {
+                setError("Invalid email or password.");
+            } else if (err.code === "auth/too-many-requests") {
+                setError("Too many failed login attempts. Account temporarily locked.");
+            } else {
+                setError("Failed to login. Please try again.");
+            }
+        }
+
+        finally {
+            setLoading(false);
+
+        }
     }
 
 
@@ -42,9 +81,12 @@ export default function Login() {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         placeholder="Enter your password"
+                        disabled={loading}
                     />
                 </div>
-                <button type="submit" className={styles.submitBtn}>Sign In</button>
+                <button type="submit" className={styles.submitBtn} disabled={loading}>
+                    {loading ? "Signing In..." : "Sign In"}
+                </button>
             </form>
         </div>
     )
